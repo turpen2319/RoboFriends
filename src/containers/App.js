9a4534d1dbc026f2a315b6ac0676623a130033fd
Your Name <you@example.com>
2021-02-23 01:37:30 +0900

    test

--- src/containers/App.js
+++ src/containers/App.js
@@ -37,7 +37,7 @@ class App extends Component {
       ):
       (
         <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
+          <h1 className='f1'>RoboFriend</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
