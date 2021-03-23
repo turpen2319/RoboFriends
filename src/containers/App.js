c92a75f111872e2ba63690f3effe65a2c96c9329
Your Name <you@example.com>
2021-03-23 12:18:51 +0900

    added react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
 
-  componentDidMount() {
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
-  }
+      .then(users => setRobots(users));
+  },[]); //this second param means 'run useEffect when the stuff inside the array changes'
+        //  so if I put [searchfield], useEffect will run whenever the searchfield changes
+       //   but we wanna leave it empty so useEffect only runs one time intitially
+      //  Adding the empty array essentially makes this the same as "componentDidMount()" 
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
+  };
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !robots.length ?
-      (
-      <div className='loading'>
-        <h1>Searching For Robots...</h1>
-        <div class="lds-ripple"><div></div><div></div></div>
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
+
+  console.log(robots);
+  return !robots.length ?
+    (
+    <div className='loading'>
+      <h1>Searching For Robots...</h1>
+      <div className="lds-ripple"><div></div><div></div></div>
+    </div>
+    ):
+    (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
-      ):
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
       );
-  }
+  
 }
 
 export default App;
